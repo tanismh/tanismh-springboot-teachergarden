0bc7f8b598db7f0789911679699a8f3310020392
tanismh <1165810311@qq.com>
2020-12-06 17:24:19 +0800

    返回数据模型“

--- src/main/java/com/hwb/tg/Model/CodeEnum.java
+++ src/main/java/com/hwb/tg/Model/CodeEnum.java
@@ -6,4 +6,41 @@ package com.hwb.tg.Model;
  */
 
 public enum CodeEnum {
+    SUCCESS(200, "处理成功"),
+    FAILD(400, "处理失败"),
+    FAILD_LOGIN(444, "登录失败"),
+    NOT_LOGIN(402,"暂未登录"),
+    API_PARAMETER_ERROR(500, "参数错误"),
+    UNEXPECTED_ERROR(999, "未知错误"),
+    REFULSE_REQUEST(777, "拒绝访问");
+
+    /**
+     * 状态码
+     */
+    private int code;
+    /**
+     * 消息
+     */
+    private String msg;
+
+    CodeEnum(Integer code, String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/com/hwb/tg/Model/ReturnModel.java
+++ src/main/java/com/hwb/tg/Model/ReturnModel.java
@@ -1,9 +1,73 @@
 package com.hwb.tg.Model;
 
+import com.sun.org.apache.bcel.internal.classfile.Code;
+
 /**
  * @author 何伟斌
  * @date 2020/12/6 12:00
  */
 
 public class ReturnModel {
+    /**
+     * 状态码
+     */
+    public Integer code;
+    /**
+     * 消息
+     */
+    public String msg;
+    /**
+     * 返回数据
+     */
+    public Object data;
+
+    /**
+     * 无返回数据构造器
+     * @param codeEnum
+     */
+    ReturnModel(CodeEnum codeEnum){
+        this.code = codeEnum.getCode();
+        this.msg = codeEnum.getMsg();
+    }
+
+    /**
+     * 有返回数据构造器
+     * @param codeEnum
+     * @param data
+     */
+    ReturnModel(CodeEnum codeEnum, Object data){
+        this.code = codeEnum.getCode();
+        this.msg = codeEnum.getMsg();
+        this.data = data;
+    }
+
+
+    /**
+     * get/set方法
+     * @return
+     */
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
